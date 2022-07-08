97264655e19c88bc1580873ba0b1edfda926c6d3
deklerckm <deklerckm@hermeszsoft.hu>
2022-07-08 21:49:17 +0200

    Added missing endpoints to backend - added getElement - added deleteElement - added updateElement

--- backend/src/controllers/elementController.js
+++ backend/src/controllers/elementController.js
@@ -1,8 +1,7 @@
 import '../models/db.js';
 import Element from '../models/element.js';
 
-const listElements = async (req, res) => {
-
+const getElements = async (req, res) => {
     const { limit = 10, page = 0, searchString, category, summary } = req.query;
     const skip = page * limit;
 
@@ -19,18 +18,25 @@ const listElements = async (req, res) => {
     };
 
 
+    let count;
+    let elements;
     try {
-        const elements = await Element
+        elements = await Element
             .find(query)
             .limit(+limit)
             .skip(skip);
 
-        const count = await Element.find(query).countDocuments().exec();
+        count = await Element.find(query).countDocuments().exec();
 
-        res.json({ nodes: elements, aggregate: { count } });
     } catch (error) {
-        res.status(400).json({ message: error })
+        res.status(500).json({ message: error })
     }
+
+    res.status(200).json({ nodes: elements, aggregate: { count } });
+}
+
+const getElement = async (req, res) => {
+    res.status(200).json(res.element);
 }
 
 const createElement = async (req, res) => {
@@ -44,10 +50,43 @@ const createElement = async (req, res) => {
 
     try {
         await newElement.save();
-        res.json(newElement);
     } catch (error) {
-        res.status(400).json({ message: error })
+        res.status(400).json({ message: error.message })
     }
+
+    res.status(200).json(newElement);
 }
 
-export { listElements, createElement };
+const deleteElement = async (req, res) => {
+    try {
+        res.element.remove();
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+    res.status(200).json(res.element);
+}
+
+const updateElement = async (req, res) => {
+    const properties = ['summary', 'category', 'sum', 'currency', 'paid'];
+
+    for (let i = 0; i < properties.length; i++) {
+        const propertyKey = properties[i];
+
+        if (req.body[propertyKey]) {
+            res.element[propertyKey] = req.body[propertyKey];
+        }
+    }
+
+    let updatedSubscriber;
+
+    try {
+        updatedSubscriber = await res.element.save();
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
+    res.status(200).json(updatedSubscriber);
+}
+
+export { getElements, getElement, createElement, deleteElement, updateElement };
